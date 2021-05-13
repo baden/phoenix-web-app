aa5e57bdb8e656583797a096605ff32cee58a295
Denys Batrak <baden.i.ua@gmail.com>
2021-05-14 02:00:24 +0300

    Распарсили одну бинарную запись.

--- src/API/GPS.elm
+++ src/API/GPS.elm
@@ -1,7 +1,7 @@
 module API.GPS exposing (..)
 
 import Bytes exposing (Endianness(..))
-import Bytes.Decode as Decode exposing (Decoder)
+import Bytes.Decode as Decode exposing (Decoder, andThen, unsignedInt16, unsignedInt32, unsignedInt8)
 import Http
 
 
@@ -10,8 +10,12 @@ type Msg
 
 
 type alias Data =
-    { lat : Float
-    , lon : Float
+    { fsourse : Int
+    , sats : Int
+    , dt : Int
+    , latLng : LatLng
+    , speed : Float
+    , dir : Int
     }
 
 
@@ -25,7 +29,7 @@ getBinTrack from to =
     Http.riskyRequest
         { method = "GET"
         , headers = [ Http.header "Accept" "application/octet-stream" ]
-        , url = "http://pil.fx.navi.cc/1.0/geos/ODY3NTU2MDQ2MTkxOTE1?from=450045&to=450188&access_token=UJs7TyiOEp83x48EAhzlKMoxZdYnhUZQ"
+        , url = "http://pil.fx.navi.cc/1.0/geos/ODY3NTU2MDQ2MTkxOTE1?from=450045&to=450188&access_token=6sAYC4AZ6Hnlawd1fwskF135X9PG7sGV"
         , body = Http.emptyBody
         , expect = e
         , timeout = Nothing
@@ -35,10 +39,96 @@ getBinTrack from to =
 
 
 -- |> Cmd.map APIGpsMsg
+-- Пока выделим только часть полей
 
 
 dataDecoder : Decoder Data
 dataDecoder =
-    Decode.map2 Data
-        (Decode.float32 BE)
-        (Decode.float32 BE)
+    unsignedInt16 LE
+        |> andThen
+            (\header ->
+                let
+                    _ =
+                        Debug.log "Decode" header
+                in
+                if header == 0xF5FF then
+                    decodeBody
+
+                else
+                    Decode.fail
+             -- Decode.succeed (Data 0 0 0.0 0.0)
+            )
+
+
+
+-- Выглядит это конечно монструозно
+
+
+decodeBody : Decoder Data
+decodeBody =
+    unsignedInt8
+        |> andThen
+            (\fsource ->
+                unsignedInt8
+                    |> andThen
+                        (\sats ->
+                            unsignedInt32 LE
+                                |> andThen
+                                    (\dt ->
+                                        decodeLatLon
+                                            |> andThen
+                                                (\latLng ->
+                                                    unsignedInt16 LE
+                                                        |> andThen
+                                                            (\speed ->
+                                                                unsignedInt16 LE
+                                                                    |> andThen
+                                                                        -- Not used now
+                                                                        (\alt ->
+                                                                            unsignedInt8
+                                                                                |> andThen
+                                                                                    (\dir ->
+                                                                                        Decode.bytes 11
+                                                                                            |> andThen
+                                                                                                (\rest ->
+                                                                                                    Decode.succeed (Data fsource sats dt latLng (toFloat speed * 1.852 / 100.0) (dir * 2))
+                                                                                                )
+                                                                                    )
+                                                                        )
+                                                            )
+                                                )
+                                    )
+                        )
+            )
+
+
+
+-- (Decode.float32 BE)
+-- (Decode.float32 BE)
+
+
+type alias LatLng =
+    { lat : Float
+    , lng : Float
+    }
+
+
+decodeLatLon : Decoder LatLng
+decodeLatLon =
+    -- unsignedInt32 LE
+    --     |> andThen
+    --         (\lat ->
+    --             unsignedInt32 LE
+    --                 |> andThen
+    --                     (\lng ->
+    --                         Decode.succeed <| LatLng (toFloat lat / 600000) (toFloat lng / 600000)
+    --                     )
+    --         )
+    Decode.map2 iLatLng
+        (unsignedInt32 LE)
+        (unsignedInt32 LE)
+
+
+iLatLng : Int -> Int -> LatLng
+iLatLng lat lng =
+    LatLng (toFloat lat / 600000) (toFloat lng / 600000)
